77e57fb52f7fbe3a468455ecea560414d1198073
kgbarroga <kgbarroga@mmsu.edu.ph>
2022-04-11 10:34:25 +0800

    add-method-overloading

--- src/App.java
+++ src/App.java
@@ -1,5 +1,53 @@
 public class App {
+    /**
+     * Method that adds 2 numbers (int)
+     */
+    private static int sum2Numbers(int num1, int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * Method that adds 3 numbers (int)
+     * 
+     */
+    private static int sum3Numbers(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    /**
+     * 
+     * Overloading methods
+     */
+
+    /**
+     * 
+     * Compute sum of two numbers (int)
+     */
+    private static int computeSum(int num1, int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * 
+     * Compute sum of three numbers (int)
+     */
+    private static int computeSum(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Overloading....");
+        /**
+         * Invoke Methods
+         */
+        System.out.println("The sum of 25 and 50 is " + App.sum2Numbers(25, 50));
+        System.out.println("The sum of 25 and 50 and 100 is " + App.sum3Numbers(25, 50, 100));
+        System.out.println("==================");
+        /**
+         * Invoking methods by overloading
+         */
+        System.out.println("The sum of 25 and 50 is " + App.computeSum(25, 50));
+        System.out.println("The sum of 25 and 50 and 100 is " + App.computeSum(25, 50, 100));
     }
 }
